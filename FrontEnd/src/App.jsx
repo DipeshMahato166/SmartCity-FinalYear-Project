333f79ed02dbbe75cf89233cdabc9ae7840bb414
raja <rajakumarshah216@gmail.com>
2026-06-29 01:47:41 +0545

    using axios, redux, reduxjs/toolkit

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,27 +6,28 @@ import HeroSec from "./component/global/home/Herosec"
 import AdminHomePage from './layout/AdminHomePage'
 import UserManagement from './component/Admin/UserManagement'
 
+import { Provider } from "react-redux";
+import store from './redux/store';
+
 const App = () => {
 
   return (
-    // <div>
-    //   <RouterProvider router={route} />
-    //   <Herosec/>
-    // </div>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomeLayout />}>
-          {/* User Layout */}
-          <Route index element={<HeroSec />} />
-        </Route>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomeLayout />}>
+            {/* User Layout */}
+            <Route index element={<HeroSec />} />
+          </Route>
 
-        <Route path='/admin' element={<AdminLayout />}>
-          {/* Admin Layout */}
-          <Route index element={<AdminHomePage />} />
-          <Route path='users' element={<UserManagement />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path='/admin' element={<AdminLayout />}>
+            {/* Admin Layout */}
+            <Route index element={<AdminHomePage />} />
+            <Route path='users' element={<UserManagement />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
